d2738200004ae9b203d8626ed75f8d19f9983c11
Biljana Zivkovic <77129646+biljanazivkovic@users.noreply.github.com>
2021-01-08 16:40:21 +0100

    Add files via upload

---
+++ Program1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ex20
+{
+    interface IFirstInterface
+    {
+        void myMethod();
+    }
+
+    interface ISecondInterface
+    {
+        void myOtherMethod();
+    }
+
+    class DemoClass : IFirstInterface, ISecondInterface
+    {
+        public void myMethod()
+        {
+            Console.WriteLine("Some text...");
+        }
+
+        public void myOtherMethod()
+        {
+            Console.WriteLine("Some other text...");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DemoClass myObj = new DemoClass();
+            myObj.myMethod();
+            myObj.myOtherMethod();
+        }
+    }
+}
