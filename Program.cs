369a7f961e02b58b4122463ee19fcd52c35d5ba7
Biljana Zivkovic <77129646+biljanazivkovic@users.noreply.github.com>
2021-01-08 16:38:11 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ex19
+{
+    interface IAnimal
+    {
+        void animalSound();
+    }
+
+    class Pig : IAnimal
+    {
+        public void animalSound()
+        {
+            Console.WriteLine("The pig says: wee wee");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pig myPig = new Pig();
+            myPig.animalSound();
+        }
+    }
+}
